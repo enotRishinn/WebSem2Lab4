2b4fd3654c53b3e486f0936865a516b34cc7006d
Victoria Emtseva <enot.rishinn@yandex.com>
2019-11-05 01:03:51 +0300

    ok

--- src/Geolocation.js
+++ src/Geolocation.js
@@ -9,30 +9,29 @@ class Geolocation extends React.Component {
     this.getGeolocation();
   }
 
-render() {
-  return (
-    <div className="header">
-      <div className="headerText">Weather here</div>
-      <button className="headerButton" onClick={() => this.getGeolocation()}>Update Geolocation</button>
-    </div>
-  );
-}
-
-
-getGeolocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.props.loadGeolocation({lat: position.coords.latitude, lon: position.coords.longitude});
-      this.props.fetchWeatherByCoords({lat: position.coords.latitude, lon: position.coords.longitude});
-    },
-    () => {
-      this.props.loadGeolocation({lat: 55.45, lon: 37.36});
-      this.props.fetchWeatherByCoords({lat: 55.45, lon: 37.36});
-    });
-  } else {
-    this.props.fetchGeolocationError('your browser does not support geolocation');
+  render() {
+    return (
+      <div className="header">
+        <div className="headerText">Weather here</div>
+        <button className="headerButton" onClick={() => this.getGeolocation()}>Update Geolocation</button>
+      </div>
+    );
   }
-}
+
+  getGeolocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.props.loadGeolocation({lat: position.coords.latitude, lon: position.coords.longitude});
+        this.props.fetchWeatherByCoords({lat: position.coords.latitude, lon: position.coords.longitude});
+      },
+      () => {
+        this.props.loadGeolocation({lat: 55.45, lon: 37.36});
+        this.props.fetchWeatherByCoords({lat: 55.45, lon: 37.36});
+      });
+    } else {
+      this.props.fetchGeolocationError('your browser does not support geolocation');
+    }
+  }
 }
 
 
