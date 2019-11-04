bf183c4bac3b9b740a961392a48a1d10c5a2152f
Victoria Emtseva <enot.rishinn@yandex.com>
2019-11-04 22:30:05 +0300

    actionreducer

--- src/Geolocation.js
+++ src/Geolocation.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './Geolocation.css';
 
-function Geolocation() {
+class Geolocation extends React.Component {
+  componentDidMount() {
+    this.getGeolocation();
+  }
+
+render() {
   return (
     <div className="header">
       <div className="headerText">Weather here</div>
       <button className="headerButton">Update Geolocation</button>
     </div>
   );
+}
+}
+
+const getGeolocation = () => {
+
 }
 
 export default Geolocation;
