a86f764f3998e131fa06184e04388cd3acf5ef2b
Victoria Emtseva <enot.rishinn@yandex.com>
2019-11-05 19:23:44 +0300

    addedcity reducer and action

---
+++ src/actions/fetchWeatherByCityName.js
@@ -0,0 +1,17 @@
+export function fetchWeatherByCityName(cityName) {
+ return function(dispatch) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7825ce4ffa896c5019e53087c858568a&units=metric&lang=en`)
+      .then(response => {
+        response.json()
+          .then(json => {
+            if (response.ok) {
+              dispatch(fetchAddedCitiesSuccess(json));
+            } else {
+              let error = json.message;
+              dispatch(fetchAddedCitiesError(error));
+            }
+          });
+      },
+      error => dispatch(fetchAddedCitiesError(error)))
+  }
+}
