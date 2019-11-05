0be1762cc75bd8debb5ddf0b66e482745ef88420
Victoria Emtseva <enot.rishinn@yandex.com>
2019-11-05 19:52:47 +0300

    store

--- src/store.js
+++ src/store.js
@@ -3,11 +3,17 @@ import geolocationReducer from './reducers/geolocationReducer';
 import addedCitiesReducer from './reducers/addedCitiesReducer';
 import thunk from "redux-thunk";
 
+const LOCAL_STORAGE_KEY = 'added_cities';
+
 const reducer = combineReducers({
 geolocation : geolocationReducer,
 added_cities : addedCitiesReducer,
 })
 
 const store = createStore (reducer, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify([...store.getState().added_cities.added_cities.keys()]));
+});
 
 export default store;
