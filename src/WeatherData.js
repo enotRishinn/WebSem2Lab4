82ff63a7f826005e7bbd71b02722b3f2fcd6bbac
Victoria Emtseva <enot.rishinn@yandex.com>
2019-11-05 15:19:23 +0300

    styles

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -6,16 +6,16 @@ class WeatherData extends React.Component {
     return (
       <div>
         <div className="leftWeather">
-          <div className="City>City": {this.props.data.name}</div>
-          <div className="Temp">Temperature: {this.props.data.main.temp}</div>
+          <div className="City">City: {this.props.data.name}</div>
+          <div className="Temp">Temperature: {Math.round(this.props.data.main.temp)} °C</div>
           <img className="Icon" src={`https://openweathermap.org/img/wn/${this.props.data.weather[0].icon}.png`} />
         </div>
 
         <div className="rightWeather">
-          <div className="Pressure">Pressure: {this.props.data.main.pressure}</div>
-          <div className="Humidity">Humidity: {this.props.data.main.humidity}</div>
-          <div className="Clouds">Clouds: {this.props.data.clouds.all}</div>
-          <div className="Wind">Wind: {this.props.data.wind.speed}, {this.props.data.wind.deg}</div>
+          <div className="Pressure">Pressure: {this.props.data.main.pressure} hPa</div>
+          <div className="Humidity">Humidity: {this.props.data.main.humidity}%</div>
+          <div className="Clouds">Clouds: {this.props.data.clouds.all}%</div>
+          <div className="Wind">Wind: {this.props.data.wind.speed} m/s</div>
           <div className="Coords">Coordinates: [{this.props.data.coord.lon}, {this.props.data.coord.lat}]</div>
         </div>
       </div>
