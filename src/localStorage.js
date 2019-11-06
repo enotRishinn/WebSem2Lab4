8025fa83b4bea0ecccd8f7d16eed3a470366d98b
= <=>
2019-11-06 23:33:06 +0300

    OKKK

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,9 +1,10 @@
-export const LOCAL_STORAGE_KEY = "favorites";
+export const LOCAL_STORAGE_KEY = "added_cities";
 
 export default function getAddedCitiesFromStorage(key = LOCAL_STORAGE_KEY) {
-  const localStorageContent = JSON.parse(localStorage.getItem(key));
+  const localValue = localStorage.getItem(key);
+  const localStorageContent = JSON.parse(localValue);
   let favorites = [];
   if (localStorageContent !== null && Array.isArray(localStorageContent))
     favorites = localStorageContent;
-  return new Map(favorites.map(cityName => [cityName, undefined]));
+  return new Map(favorites.map(cityName => [cityName]));
 }
